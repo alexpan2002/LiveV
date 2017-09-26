7faff1c1d9cb7cecb1f7b6d5bb8bf42d916be576
Alex Pan <alexpan2002@msn.com>
2017-09-26 12:17:44 +0800

    socket listen port

--- node/app.js
+++ node/app.js
@@ -5,6 +5,7 @@ var io = require('socket.io')(server)
 //server.listen(5000)
 
 app.set('port', process.env.PORT || 5000);
+io.listen(process.env.PORT);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
